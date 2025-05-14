f0a7b795005925cca6ab451753b8b8f12d15bd2a
luisillo77D <luisillo77mario@gmail.com>
2025-05-13 17:46:04 -0700

    Refactor database connection to use environment variables for configuration

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('chafafy', 'root', '', {
-  host: 'localhost',
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
 });
